e2145dcc88f5ee5a5ef971a9968e90c1339d3154
Gustavo C <p8queen@gmail.com>
2012-04-27 09:05:19 -0300

    escribe hola

--- cliente.c
+++ cliente.c
@@ -7,14 +7,14 @@
 #include <strings.h>
 
 #define PORT 5001
-#define MAXDATASIZE 100
+#define MAXDATASIZE 254
 
 //espera argv[1] --> 127.0.0.1
 int main(int argc, char *argv[])
 {
     int fd, numbytes;
     char buf[MAXDATASIZE];
-    struct hostent *he;
+//    struct hostent *he;
     struct sockaddr_in server;
 
     if (argc!=2) 
@@ -22,35 +22,41 @@ int main(int argc, char *argv[])
     printf("Usa:[%s]<Direccion IP>\n", argv[1]);
     exit(1);
     }
-
+/*
     if ( (he=gethostbyname(argv[1]) ) == NULL)
     {
         printf("error en gethosbyname\n" );
         exit(1);
     }
-
+*/
+    if(!(inet_aton(argv[1], &server.sin_addr))){
+        perror("ip invalida\n"); exit(1);
+    }
+    server.sin_family=AF_INET;
+    server.sin_port=htons(PORT);
+    //server.sin_addr=*((struct in_addr*) he->h_addr); //ip
+    bzero(&(server.sin_zero),0);
+    
     if ( (fd=socket(AF_INET,SOCK_STREAM,0)) == -1)
     {
         printf("error en socket\n" );
         exit(1);
     }
 
-    server.sin_family=AF_INET;
-    server.sin_port=htons(PORT);
-    server.sin_addr=*((struct in_addr*) he->h_addr);
-    bzero(&(server.sin_zero),0);
-
+    
     if (connect(fd,(struct sockaddr*)&server, sizeof(struct sockaddr))==-1)
     {
-    printf("error en connect\n" );
+    printf("error en coneccion con socket servidor\n" );
     exit(1);
+    }else{
+        write(fd,"hola\n",5);
     }
 
     if ( (numbytes=recv(fd,buf,MAXDATASIZE,0))==-1)
     {
-    printf("error en recvn" );
-    exit(-1);
-    }
+        printf("error en recvn" );
+        exit(-1);
+        }
 
     buf[numbytes]='\0';
     printf("Mensaje del servidor:%s\n",buf);
